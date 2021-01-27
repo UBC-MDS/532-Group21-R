c1400b4a81c1b091ddb1ffcfba419602ce5aa977
Syad Khan <syad@student.ubc.ca>
2021-01-27 17:12:25 -0500

    Combine plot codes into one file

--- src/utils/ggplot-line.R
+++
@@ -1,6 +0,0 @@
-library(ggplot2)
-library(tidyverse)
-library(plotly)
-library(here)
-
-df <- read_csv('../../data/processed/gapminder_processed.csv')
---
+++ src/utils/plot_code.R
@@ -0,0 +1,44 @@
+library(here)
+library(tidyverse)
+
+df <- read_csv(here("data", "processed", "gapminder_processed.csv"))
+
+
+get_topbtm_data <- function(data, stat, year_list = c(1968, 2015)){
+  top_countries <- data %>% filter(year == year_list[2]) %>%
+    arrange(desc(!!sym(stat))) %>%
+    slice(1:5) %>%
+    pull(country)
+  
+  data_return <- data %>% filter(country %in% top_countries)
+}
+
+
+plot_line <- function(stat = "children_per_woman"){
+  data <- get_topbtm_data(df, stat)
+  
+  line_plot <- ggplot(countries, aes(x = year,
+                                   y = !!sym(stat),
+                                   color = country)) + 
+    geom_line()
+  
+  line_plot
+}
+
+plot_bar <- function(stat = "children_per_woman") {
+  data <- get_topbtm_data(df, stat)
+  
+  bar_plot <- ggplot(data, aes(x = !!sym(stat),
+                               y = reorder(country, !!sym(stat)),
+                               color = country,
+                               name = country,
+                               count = !!sym(stat))) + 
+    geom_bar(stat = "identity") + 
+    ylab("country")
+  bar_plot
+}
+
+plot_line(stat = "child_mortality")
+
+plot_bar(stat = "child_mortality")
+
